e03b573769bdbabdece588bddffcdb342abbe301
mrtlive <mrtlive1@gmail.com>
2022-12-02 22:26:35 +0300

    app intro slider added

---
+++ appStyle.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+import { W, H } from "./src/utils/ui/dimension";
+import colorPalette from "./src/themes/colors";
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  lottieWrapper: {
+    overflow: "hidden",
+    borderRadius: 250,
+  },
+  lottieView: {
+    width: W(100),
+  },
+  image: {
+    width: 320,
+    height: 320,
+    marginVertical: 32,
+  },
+  text: {
+    color: colorPalette.lightRed,
+    fontWeight: "400",
+    fontSize: 18,
+    textAlign: "center",
+    padding: H(5),
+  },
+  buttonCircle: {
+    bottom: W(5),
+  },
+});
+export default styles;
