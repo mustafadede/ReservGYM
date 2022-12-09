2fa6df86f882a75fba9d1ecda524bc751e5e1b79
mrtlive <mrtlive1@gmail.com>
2022-12-09 22:14:20 +0300

    lottie loading animation

--- src/screens/MemberProfile/index.js
+++ src/screens/MemberProfile/index.js
@@ -10,61 +10,75 @@ import { spacing } from "../../configs";
 import { Member, MemberInfo, Spacing, HeaderBar, LinearButton } from "../../components";
 import styles from "./style";
 import colorPalette from "../../themes/colors";
+import LottieFile from "lottie-react-native";
+import { SLEEP } from "../../utils/sleep";
 
 const MemberProfile = ({ navigation }) => {
-
   const { userId } = useSelector((state) => state.app);
-
-  const [user, setUser] = useState({});
+  const [loaded, setLoaded] = useState(false);
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
-    let rootRef = database().ref();
-    rootRef
-      .child("Users")
-      .orderByChild("userid")
-      .equalTo(userId)
-      .once("value")
-      .then((snapshot) => {
-        const key = Object.keys(snapshot.val())[0];
-        setUser(snapshot.val()[key]);
-      });
+    const loadUserDataFromFirebase = () => {
+      let rootRef = database().ref();
+      rootRef
+        .child("Users")
+        .orderByChild("userid")
+        .equalTo(userId)
+        .once("value")
+        .then((snapshot) => {
+          console.log("işlem başladı");
+          const key = Object.keys(snapshot.val())[0];
+          setUser(snapshot.val()[key]);
+        })
+        .then(async () => {
+          await SLEEP(1500);
+          setLoaded(true);
+        });
+    };
+
+    loadUserDataFromFirebase();
   }, []);
 
   return (
     <>
       <HeaderBar title={"Member Profile"} exit onClickHandler={() => navigation.navigate("Login")} onClickBackHandler={() => navigation.goBack()} />
-      <View style={styles.main}>
-        <Spacing spacing={spacing.xs} />
-        <Member style={styles.member}
-          memberName={user.name + " " + user.surname}
-          memberAllowedTime={user.allowedProfileTime}
-        />
-        <Spacing spacing={spacing.s} />
-        <MemberInfo status={user.status} />
-        <View style={styles.buttonLinearContainer}>
-          <View style={styles.buttonLinear}>
-            <LinearButton
-              colors={[colorPalette.lightRed, colorPalette.darkRed]}
-              title={"Rezervasyonlarım"}
-              onClickHandler={() => navigation.navigate("Reservation")}
-            />
-          </View>
-          <View style={styles.buttonLinear}>
-            <LinearButton
-              colors={[colorPalette.lightRed, colorPalette.darkRed]}
-              title={"Ödeme Yap"}
-              onClickHandler={() => navigation.navigate("PaymentScreen")}
-            />
-          </View>
+      {!loaded && (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <LottieFile source={require("../../assets/129733-dancing-girl.json")} autoPlay />
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate("QrCodeScreen")}>
-          <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={[colorPalette.lightRed, colorPalette.darkRed]} style={styles.buttonCircular}>
-            <Text style={{ color: colorPalette.white, fontWeight: "bold" }}>
-              <Icon name="camera" size={30} />
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
+      )}
+      {loaded && (
+        <View style={styles.main}>
+          <Spacing spacing={spacing.xs} />
+          <Member style={styles.member} memberName={user.name + " " + user.surname} memberAllowedTime={user.allowedProfileTime} />
+          <Spacing spacing={spacing.s} />
+          <MemberInfo status={user.status} />
+          <View style={styles.buttonLinearContainer}>
+            <View style={styles.buttonLinear}>
+              <LinearButton
+                colors={[colorPalette.lightRed, colorPalette.darkRed]}
+                title={"Rezervasyonlarım"}
+                onClickHandler={() => navigation.navigate("Reservation")}
+              />
+            </View>
+            <View style={styles.buttonLinear}>
+              <LinearButton
+                colors={[colorPalette.lightRed, colorPalette.darkRed]}
+                title={"Ödeme Yap"}
+                onClickHandler={() => navigation.navigate("PaymentScreen")}
+              />
+            </View>
+          </View>
+          <TouchableOpacity onPress={() => navigation.navigate("QrCodeScreen")}>
+            <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={[colorPalette.lightRed, colorPalette.darkRed]} style={styles.buttonCircular}>
+              <Text style={{ color: colorPalette.white, fontWeight: "bold" }}>
+                <Icon name="camera" size={30} />
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      )}
     </>
   );
 };
